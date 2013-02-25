c13e4fa526b7169d144c0674ff72ca6df3b08e95
Mitchel Kuijpers <m.kuijpers@avisi.nl>
2013-02-25 10:41:23 +0100

    created a very simple client

---
+++ gwt-client/src/main/java/nl/avisi/client/widget/PizzaInputWidget.java
@@ -0,0 +1,11 @@
+package nl.avisi.client.widget;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mitchel
+ * Date: 2/24/13
+ * Time: 4:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PizzaInputWidget {
+}
